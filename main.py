996e6262a069066ec23e1493d456bafad13bc8f5
John Roddy <johnroddy16@yahoo.com>
2025-02-21 01:14:16 -0800

    bug fixes

--- main.py
+++ main.py
@@ -6,51 +6,50 @@ import smtplib
 from time import sleep 
 
 MY_LAT = 47.606209 # seattle latitude
-MY_LONG = -122.332069 # seattle longitude   
+MY_LONG = -122.332069 # seattle longitude  
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json')
-response.raise_for_status()
-data = response.json()
+email = 'nohtyp742@gmail.com'
+password = 'xddewbafczduzurt'    
 
-iss_latitude = float(data['iss_position']['latitude'])
-iss_longitude = float(data['iss_position']['longitude']) 
-
-parameters = {
+# dark: 
+def dark():
+    parameters = {
     'lat': MY_LAT,
     'lng': MY_LONG,
     'formatted': 0,
-}
-
-response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data['results']['sunrise'].split('T')[1].split(':')[0]) - 8 # time comes back in UTC
-sunset = int(data['results']['sunset'].split('T')[1].split(':')[0]) + 16 
-
-time_now = dt.now().hour  
-
-# dark: 
-def dark(cur_time, sett, rise):
-    if cur_time >= sett or cur_time <= rise:
-        return True 
-    return False 
+    }
+    
+    response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data['results']['sunrise'].split('T')[1].split(':')[0]) - 8 # time comes back in UTC
+    sunset = int(data['results']['sunset'].split('T')[1].split(':')[0]) + 16
+    
+    time_now = dt.now().hour
+    
+    if time_now >= sunset or time_now  <= sunrise:
+        return True  
 
 # close:
-def close(lat, lng, iss_lat, iss_lng):
-    if lat - 5 <= iss_lat <= lat + 5 and lng - 5 <= iss_lng <= lng + 5:
-        return True 
-    return False 
+def close():
+    response = requests.get(url='http://api.open-notify.org/iss-now.json')
+    response.raise_for_status()
+    data = response.json()
 
-dark = dark(time_now, sunset, sunrise)
-close = close(MY_LAT, MY_LONG, iss_latitude, iss_longitude)
+    iss_latitude = float(data['iss_position']['latitude'])
+    iss_longitude = float(data['iss_position']['longitude'])
+    
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True  
 
 # make the program run every 60 seconds:
 while True:
-    sleep(60)
-    if dark and close:
-        email = 'nohtyp742@gmail.com'
-        password = 'not a real password'
-        
+    sleep(60) 
+    
+    darkk = dark()
+    closee = close()
+    
+    if darkk and closee:
         with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
             # make the connection secure:
             connection.starttls()  
@@ -58,3 +57,4 @@ while True:
             connection.login(user=email, password=password)
             # send the email: 
             connection.sendmail(from_addr=email, to_addrs='johnroddy.16@gmail.com', msg=f'subject:ISS!!!!\n\nLook Up!!!!')
+    
